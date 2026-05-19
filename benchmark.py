637da96a6278acc58fdb6b685b831076441f2db1
ianif <ioannaanif@gmail.com>
2026-05-19 11:10:36 +0100

    init commit

---
+++ benchmark.py
@@ -0,0 +1,24 @@
+import time
+  import random
+  from sorter import sort_numbers
+  
+  data = random.sample(range(100000), 5000)
+
+  times = []
+  for _ in range(5):
+      sample = data.copy()
+      start = time.perf_counter()
+      sort_numbers(sample)
+      end = time.perf_counter()
+      times.append((end - start) * 1000)
+      
+  avg_ms = sum(times) / len(times)
+  
+  results = {
+      "execution_time_ms": round(avg_ms, 3),
+      "throughput_ops_per_sec": round(5000 / (avg_ms / 1000), 1),
+      "worst_case_ms": round(max(times), 3)
+  }
+ 
+  with open("artemis_results.json", "w") as f:
+      json.dump(results, f)
